3017c6dfbba4d691656f67eb555bddab4ab8c79f
tobynet <toby.netinfo+git@gmail.com>
2018-06-12 11:36:31 +0900

    Fix comments

--- Exercise-3/sensor_stick/scripts/object_recognition.py
+++ Exercise-3/sensor_stick/scripts/object_recognition.py
@@ -19,15 +19,16 @@ from sensor_stick.msg import DetectedObject
 from sensor_stick.pcl_helper import XYZRGB_to_XYZ, get_color_list, ros_to_pcl, pcl_to_ros, rgb_to_float
 
 import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2, PointField
+
 
 def voxel_filter(cloud, leaf_size = 0.01):
     # Voxel Grid filter for input point cloud
-    # ボクセルを生成
+    # Crate Voxel grid
     vox = cloud.make_voxel_grid_filter()
 
-    # ボクセルのグリッドサイズを設定
-    # 大きいほど Point Cloud の点を減らせる
-    # 1 -> increment..
+    # To obtain the downsampled point clouds 
+    # 
     # voxel size (also known as `leaf`)
     vox.set_leaf_size(leaf_size, leaf_size, leaf_size)
 
@@ -35,17 +36,15 @@ def voxel_filter(cloud, leaf_size = 0.01):
 
 def pass_through_filter(cloud, filter_axis='z', axis_limit=(0.6, 1.1)):
     """
-    欲しい範囲だけを切り取るフィルタ
-        filter_axis: 切り取る軸
-        axis_limit: テーブル下上のリミット
+    Cut off with range using PassThrough filter
+        filter_axis: axis
+        axis_limit: range of cutting off
     Returns:
-        filter
+        filter: pcl.PCLPointCloud2
     """
     # Create a PassThrough filter object.
-    # パススルー用フィルタをゲット
     passthrough = cloud.make_passthrough_filter()
 
-    # ざっくり z軸でテーブルの上だけを切り取る
     # Assign axis and range to the passthrough filter object.
     passthrough.set_filter_field_name(filter_axis)
     passthrough.set_filter_limits(axis_limit[0], axis_limit[1])
@@ -55,25 +54,25 @@ def pass_through_filter(cloud, filter_axis='z', axis_limit=(0.6, 1.1)):
 
 def ransac_plane_segmentation(cloud, max_distance = 0.010):
     """
-    RANSAC で分離する
+    Separate table and objects
+
     parameters:
-        max_distance: しきい値
+        max_distance: threshold
     return:
-        inliers, coefficients: 近い点群, 係数？
+        inliers, coefficients: pcl.PointIndices of table, pcl.ModelCoefficients
     """
 
-    # RANSAC で机とオブジェクトを分離する
     # RANSAC plane segmentation
     seg = cloud.make_segmenter()
     seg.set_model_type(pcl.SACMODEL_PLANE)
     seg.set_method_type(pcl.SAC_RANSAC)
 
-    # 判定するための、しきい値を設定する?
+    # Setup parameters
     # 1..0.1 : xx
     # 0.01.. : OK!
     seg.set_distance_threshold(max_distance)
 
-    # inlier(推定したいものに近い側の点)とモデルを得る
+    # Obtain inliers and the model
     inliers, coefficients = seg.segment()
     return  inliers, coefficients
 
@@ -83,18 +82,17 @@ def euclidean_clustering(cloud, tolerance=0.001, cluster_range=(10,250)):
     Euclidean Clustering using PCL with k-d tree
 
     returns:
-        cluster_indices: インデックス
-        white_cloud: Point Cloud XYZ only
+        cluster_indices, white_cloud: pcl.PointIndices, XYZ Point Cloud
     """
-    # XYZ情報のみの オブジェクト用 Point Cloud を取得し、 PCL 用に kd-tree に変換。
+
+    # Obtain XYZ only from XYZRGB nad convert it to kd-tree
     white_cloud = XYZRGB_to_XYZ(cloud)
     kdtree = white_cloud.make_kdtree()
 
     # Create a cluster extraction object
-    # オブジェクトの生成
     ec = white_cloud.make_EuclideanClusterExtraction()
     
-    # パラメータの設定:
+    # Setup parameters:
     # Set tolerances for distance threshold 
     # as well as minimum and maximum cluster size (in points)
     # NOTE: These are poor choices of clustering parameters
@@ -104,17 +102,15 @@ def euclidean_clustering(cloud, tolerance=0.001, cluster_range=(10,250)):
     ec.set_MaxClusterSize(cluster_range[1])
     
     # Search the k-d tree for clusters
-    # k-d tree でクラスタリングする
     ec.set_SearchMethod(kdtree)
 
     # Extract indices for each of the discovered clusters
-    # クラスタリングした点群の index を取り出す
     indices = ec.Extract()
     return indices, white_cloud
 
 
 def create_colored_cluster_cloud(cluster_indices, white_cloud):
-    # cluster 毎に色を分けつつ、 XYZ -> XYZRGB に変換。
+    # Cluster by color and convert XYZ to XYZRGB
     cluster_color = get_color_list(len(cluster_indices))
     color_cluster_point_list = []
 
@@ -146,10 +142,10 @@ def pcl_callback(pcl_msg):
     cloud_filtered = voxel_filter(cloud)
 
     # DONE: PassThrough Filter
-    # ざっくり z軸でテーブルの上だけを切り取る
+    # Assign axis and range to the passthrough filter object.
     cloud_filtered = pass_through_filter(cloud_filtered,
         filter_axis='z', axis_limit=(0.6, 1.1))
-    # テーブルの手前もオブジェクトに見えるので、切り取る
+    # Cut off the near side of the table too.
     cloud_filtered = pass_through_filter(cloud_filtered,
         filter_axis='y', axis_limit=(-10.0, -1.39))
 
@@ -157,15 +153,34 @@ def pcl_callback(pcl_msg):
     inliers, _ = ransac_plane_segmentation(cloud_filtered)
 
     # DONE: Extract inliers and outliers
-    # フィルタでオブジェクト群とテーブルを分離する(結果は ransanc の distance に依存する)
+    cloud_table = cloud_filtered.extract(inliers, negative=False)
     cloud_objects = cloud_filtered.extract(inliers, negative=True)
     rospy.loginfo(' cloud_objects: %d' % (cloud_objects.size))
 
     # DONE: Euclidean Clustering
-    # オブジェクト毎に分割する
     cluster_indices, white_cloud = euclidean_clustering(cloud_objects,
         tolerance=0.02, cluster_range=(100,15000))
     rospy.loginfo(' cluster_indices: %d, white_cloud: %d ' % (len(cluster_indices), white_cloud.size))
+
+
+    # DONE: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    cluster_cloud = create_colored_cluster_cloud(cluster_indices, white_cloud)
+    rospy.loginfo(' colored cluster: %d' % (cluster_cloud.size))
+
+    # DONE: Convert PCL data to ROS messages
+    ros_cloud_objects = pcl_to_ros(cloud_objects)
+    ros_cloud_table = pcl_to_ros(cloud_table)
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
+
+    # DONE: Publish ROS messages
+    rospy.loginfo(' Publish to objects')
+    pcl_objects_pub.publish(ros_cloud_objects)
+
+    rospy.loginfo(' Publish to table')
+    pcl_table_pub.publish(ros_cloud_table)
+
+    rospy.loginfo(' Publish to cluster_cloud')
+    pcl_cluster_pub.publish(ros_cluster_cloud)
 
     rospy.loginfo('DONE Exercise-2')
 
@@ -175,7 +190,7 @@ def pcl_callback(pcl_msg):
     detected_objects_labels = []
     detected_objects = []
 
-    # 各オブジェクトを認識する
+    # Object recognition
     for index, pts_list in enumerate(cluster_indices):
         #rospy.loginfo('  Detecting... : %d' % (index))
         # Grab the points for the cluster from the extracted outliers (cloud_objects)
@@ -200,7 +215,6 @@ def pcl_callback(pcl_msg):
         detected_objects_labels.append(label)
 
         # Publish a label into RViz
-        # ラベルを publish する
         label_pos = list(white_cloud[pts_list[0]])
         label_pos[2] += .4
         object_markers_pub.publish(make_label(label,label_pos, index))
@@ -216,8 +230,6 @@ def pcl_callback(pcl_msg):
 
     # Publish the list of detected objects
     # This is the output you'll need to complete the upcoming project!
-    # 
-    # 認識したオブジェクトを publish する
     detected_objects_pub.publish(detected_objects)
 
 
@@ -233,6 +245,15 @@ if __name__ == '__main__':
 
 
     # DONE: Create Publishers
+    pcl_objects_pub = rospy.Publisher('/pcl_objects', PointCloud2, queue_size=1)
+    rospy.loginfo('Initialize Publisher for objects')
+  
+    pcl_table_pub = rospy.Publisher('/pcl_table', PointCloud2, queue_size=1)
+    rospy.loginfo('Initialize Publisher for table')
+
+    pcl_cluster_pub = rospy.Publisher('/pcl_cluster', PointCloud2, queue_size=1)
+    rospy.loginfo('Initialize Publisher for objects cluster')
+
     # DONE: here you need to create two publishers
     # Call them object_markers_pub and detected_objects_pub
     # Have them publish to "/object_markers" and "/detected_objects" with 
